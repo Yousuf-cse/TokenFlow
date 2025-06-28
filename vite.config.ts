16f88338c84dbe213d2714f09f515fad43204339
Yousuf-cse <yousuf.mallik79@gmail.com>
2025-06-28 17:07:07 +0530

    add tailwind for pop up

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
 
-// https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        ui: resolve(__dirname, 'index.html'),
+        background: resolve(__dirname, 'src/background/background.ts'),
+        content: resolve(__dirname, 'src/content/content.ts'),
+      },
+      output: {
+        entryFileNames: (chunkInfo) => {
+          if (chunkInfo.name === 'background') return 'background.js'
+          if (chunkInfo.name === 'content') return 'content.js'
+          return 'assets/[name].js'
+        },
+      },
+    },
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
 })
